d49e56860eababb4d721642b17310940af3a1c0f
purnoroi1 <purnoroi@gmail.com>
2022-05-03 21:11:05 +0900

    파일 전체 읽어서 스트링 리스트로 저장 / 스트링 리스트 전체 파일로 저장 추가 / 특정 경로 속 파일 찾기 추가

--- C#/FileIO/FileIO/Program.cs
+++ C#/FileIO/FileIO/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.IO;
-
+using System.Collections;
+using System.Collections.Generic;
 namespace FileIO {
     class Program {
         static void Main(string[] args) {
+
             //파일 생성 & 쓰기
             // case1
             FileStream fs = new FileStream("test.txt", FileMode.Create);
@@ -49,10 +51,56 @@ namespace FileIO {
 
 
 
+            //파일 전체 읽어서 string List로 받아오기
+            //List 사용위해서 
+            //using System.Collections;
+            //using System.Collections.Generic;
+            //두가지 선언 필수
+            List<string> contents = new List<string>();
+            string filePath = "test.txt";
+            using (StreamReader sr2 = new StreamReader(new FileStream(filePath, FileMode.Open))) {
+                string line;
+                while((line = sr2.ReadLine())!=null){
+                     contents.Add(line);                
+                }
+            }
+
+            //string List안에 들어있는 스트링 수 만큼 파일에 쓰기
+
+            string filePath2 = "test.txt";
+            List<string> writeData = new List<string>();
+            writeData.Add("geasdfsdf");
+            writeData.Add("dsefxczv");
+             using (StreamWriter sw2 = new StreamWriter(new FileStream(filePath2, FileMode.Create))) {
+                foreach(string str in writeData){
+                    sw2.WriteLine(str);
+                }
+            }
+
+
+             //특정 파일 찾기
+             string target = "test.txt";
+            // 현재 프로젝트 경로
+             string Project_Path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            // 타겟 파일 경로
+            string targetPath = Project_Path;
+            // 타겟 파일 경로에 있는 모든 파일 이름들을 갖고 온 후(경로 포함)
+            string[] files = Directory.GetFiles(targetPath,"",SearchOption.AllDirectories);
+            // 해당 파일들에 대해 타겟파일과 이름이 같은 파일 출력
+            foreach(string str in files){
+                string[] paths = str.Split("\\");
+                if(target == paths[paths.Length-1]){
+                    Console.WriteLine("Target Location : " + str);
+                }
+            }
+
+
             //String Split
             string str = "국어 : 90 영어: 100 수학:83";
             string[] str_Split = str.Split(new char[] { ' ' });
             string[] str_Split2 = str.Split(' ');
+
+
 
         }
     }
