2eb3ab1aff70003b418f4220e7e7fc5f7178cb8e
purnoroi1 <purnoroi@gmail.com>
2022-05-13 15:03:35 +0900

    숫자 찾기(중복 x), 수의 범위만큼 배열을 만들어서 해결 / BinarySearch도 가능할 듯

---
+++ Java/Baekjoon_10815.java
@@ -0,0 +1,24 @@
+package Java;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Baekjoon_10815 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        long[] arr = new long[20000001];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[Integer.parseInt(st.nextToken())+10000000] = 1;
+        }
+        int k = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<k;i++){
+            bw.write(arr[Integer.parseInt(st.nextToken())+10000000] + " ");
+        }
+        bw.flush();
+
+    }
+}
