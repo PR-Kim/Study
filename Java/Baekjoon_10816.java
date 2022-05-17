7ea6f5ff31a424677fc7b2ee7edcbca087e0fae4
purnoroi1 <purnoroi@gmail.com>
2022-05-17 20:11:44 +0900

    간단함

--- Java/Baekjoon_10816.java
+++ Java/Baekjoon_10816.java
@@ -1,2 +1,30 @@
-package Java;public class Baekjoon_10816 {
+package Java;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Baekjoon_10816 {
+    public static void main(String[] args) throws IOException {
+        Baekjoon_10816 b = new Baekjoon_10816();
+        b.solution();
+
+    }
+    public void solution() throws  IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        long[] arr = new long[20000001];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[Integer.parseInt(st.nextToken()) + 10000000] +=1;
+        }
+
+        int k = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<k;i++){
+            bw.write(arr[Integer.parseInt(st.nextToken()) + 10000000] + " ");
+        }
+        bw.flush();
+    }
 }
