4c44143ec6c2897e2310c95cb8de194ffd97e101
purnoroi1 <purnoroi@gmail.com>
2022-05-17 20:12:02 +0900

    배열 정렬을 이용한 문제

--- Java/Baekjoon_10867.java
+++ Java/Baekjoon_10867.java
@@ -1,2 +1,26 @@
-package Java;public class Baekjoon_10867 {
+package Java;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baekjoon_10867 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0;i<arr.length;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        bw.write(arr[0] + " ");
+        for(int i =1;i<arr.length;i++){
+            if(arr[i-1] != arr[i])
+                bw.write(arr[i] + " ");
+        }
+        bw.flush();
+    }
 }
