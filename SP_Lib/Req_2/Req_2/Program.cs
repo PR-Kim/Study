7e93b8d78e26db322c5425e89c4d8dc07cb65690
purnoroi1 <purnoroi@gmail.com>
2022-05-19 20:54:12 +0900

    SP 2번 (콘솔에서 입력받은 명령어로 큐 생성 혹은 큐에 저장 / 출력)

---
+++ SP_Lib/Req_2/Req_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 콘솔 입력을 통해 큐 생성 및 큐에 메세지 삽입
+namespace Req_2 {
+    class Program {
+        static void Main(string[] args) {
+            Dictionary<string, Queue<string>> dictQue = new Dictionary<string, Queue<string>>();
+            Dictionary<string, int> dictSize = new Dictionary<string, int>();
+
+            string input;
+            while((input = Console.ReadLine()) != "") {
+                string[] inp = input.Split(' ');
+                if (inp[0].Equals("CREATE")) {
+                    if (dictQue.ContainsKey(inp[1])) {
+                        Console.WriteLine("Queue Exist");
+                    }
+                    else {
+                        dictQue.Add(inp[1], new Queue<string>());
+                        dictSize.Add(inp[1], int.Parse(inp[2]));
+                    }
+                }
+                else if (inp[0].Equals("SEND")) {
+                    if (dictQue[inp[1]].Count >= dictSize[inp[1]]) {
+                        Console.WriteLine("Queue Full");
+                    }
+                    else {
+                        dictQue[inp[1]].Enqueue(inp[2]);
+                    }
+                }
+                else if (inp[0].Equals("RECEIVE")) {
+                    if (dictQue[inp[1]].Count > 0) {
+                        Console.WriteLine(dictQue[inp[1]].Dequeue());
+                    }
+                }
+            }
+
+
+        }
+    }
+}
